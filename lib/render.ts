5a5f3a72420c22cb675a88e256922f02d1278869
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-08-31 12:53:49 +0800

    feat: 支持数学公式

--- lib/render.ts
+++ lib/render.ts
@@ -1,8 +1,11 @@
 import { marked } from 'marked'
 import { MarkdownPaperTheme } from './theme'
+import markedKatex from 'marked-katex-extension'
+import katexCss from 'katex/dist/katex.css?raw'
 
 export async function mdToHtml(md: string, theme: MarkdownPaperTheme): Promise<string> {
   let content: string
+  marked.use(markedKatex({ throwOnError: false }))
   content = await theme.preParseMarkdown(md)
   content = await marked(content)
   content = await theme.preParseHTML(content)
@@ -13,6 +16,7 @@ export async function mdToHtml(md: string, theme: MarkdownPaperTheme): Promise<s
       <meta charset="UTF-8">
       <title>预览</title>
       <style>${theme.css}</style>
+      <style>${`\n${katexCss}`}</style>
       <style>
         * { scrollbar-width: none; }
       </style>
